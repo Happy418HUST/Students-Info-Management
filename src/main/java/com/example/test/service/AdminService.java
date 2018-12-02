a052e9e4244af71e61f02f610f866a6503f02f62
Yu Qiao <691761071@qq.com>
2018-12-03 07:49:50 +0800

    make git not to ignore case

--- src/main/java/com/example/test/service/AdminService.java
+++ src/main/java/com/example/test/service/AdminService.java
@@ -14,19 +14,19 @@ import com.example.test.model.Admin;
 public interface AdminService {
     //sql语句里的用井号和花括号括起来的参数，对应实体类里的各个同名属性
     //例如#{userName}  对应了    参数admin对象的userName属性
-    @Select("SELECT * FROM `studentsmanagement`.`admin` where userName = #{userName} and password = #{password} ;")
+    @Select("SELECT * FROM `studentsManagement`.`admin` where userName = #{userName} and password = #{password} ;")
     Admin findByNameAndPassword(Admin admin);
 
-    @Select("SELECT * FROM `studentsmanagement`.`admin` where userName = #{userName} and password = #{password} and realName = #{realName}")
+    @Select("SELECT * FROM `studentsmMnagement`.`admin` where userName = #{userName} and password = #{password} and realName = #{realName}")
     List<Admin> findByAdminWithPage(Admin admin, int start, int end);
 
-    @Insert("INSERT INTO `studentsmanagement`.`admin` (`id`, `userName`, `password`, `realName`, `age`, `phoneNumber`, `headPicture`, `addDate`, `updateDate`, `state`) VALUES (null, #{userName}, #{password}, #{realName}, #{age}, #{phoneNumber}, #{headPicture}, now(), now(), 0);")
+    @Insert("INSERT INTO `studentsManagement`.`admin` (`id`, `userName`, `password`, `realName`, `age`, `phoneNumber`, `headPicture`, `addDate`, `updateDate`, `state`) VALUES (null, #{userName}, #{password}, #{realName}, #{age}, #{phoneNumber}, #{headPicture}, now(), now(), 0);")
     int insert(Admin admin);
 
-    @Update("UPDATE `studentsmanagement`.`admin` SET `userName` = #{userName}, `password` = #{password}, `realName` = #{realName}, `age` = #{age}, `phoneNumber` = #{phoneNumber}, `headPicture` = #{headPicture}, `updateDate` = now(), `state` = #{state} WHERE `id` = #{id};")
+    @Update("UPDATE `studentsManagement`.`admin` SET `userName` = #{userName}, `password` = #{password}, `realName` = #{realName}, `age` = #{age}, `phoneNumber` = #{phoneNumber}, `headPicture` = #{headPicture}, `updateDate` = now(), `state` = #{state} WHERE `id` = #{id};")
     int updateStateById(int id);
 
-    @Delete("DELETE FROM `studentsmanagement`.`admin` WHERE id  = #{id}")
+    @Delete("DELETE FROM `studentsManagement`.`admin` WHERE id  = #{id}")
     int deleteById(int id);
 }
 
